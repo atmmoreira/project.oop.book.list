ed42b34a6afa39fd1ac10053f692ba142d159045
Anderson Martins <atmmoreira.rj@gmail.com>
2022-08-30 22:01:59 -0300

    add: CRUD with ES5 and ES6

--- assets/js/maines6.js
+++ assets/js/maines6.js
@@ -1,19 +1,17 @@
-// Book Constructor
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
-// UI Constructor
-function UserInterface() {}
+class UserInterface {
+  addBooktoList(book) {
+    const list = document.getElementById('bookList');
 
-// Prototypes
-UserInterface.prototype.addBooktoList = function (book) {
-  const list = document.getElementById('bookList');
-
-  // Create list
-  list.innerHTML = `
+    // Create list
+    list.innerHTML = `
     <li class="list-group-item d-flex justify-content-between align-items-start">
       <div class="ms-2 me-auto">
         <div class="fw-bold">${book.title} - ${book.author}</div>
@@ -24,40 +22,38 @@ UserInterface.prototype.addBooktoList = function (book) {
       </span>
     </li>
   `;
-};
-
-// Show Alert
-UserInterface.prototype.showAlert = function (message, className) {
-  // Create div
-  const div = document.createElement('div');
-  // Add class
-  div.className = `alert alert-${className}`;
-  // Add textNode
-  div.appendChild(document.createTextNode(message));
-
-  // Get div messages and append
-  const messagesDiv = document.querySelector('.messages');
-  messagesDiv.appendChild(div);
-
-  // Timout afeter 3 seconds
-  setTimeout(function () {
-    document.querySelector('.alert').remove();
-  }, 3000);
-};
-
-// Clear Fields
-UserInterface.prototype.clearFields = function () {
-  document.getElementById('bookTitle').value = '';
-  document.getElementById('bookAuthor').value = '';
-  document.getElementById('bookIsbn').value = '';
-};
-
-// Delete Book
-UserInterface.prototype.deleteBook = function (target) {
-  if (target.classList.contains('deleteBtn')) {
-    target.parentElement.parentElement.remove();
   }
-};
+
+  showAlert(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add class
+    div.className = `alert alert-${className}`;
+    // Add textNode
+    div.appendChild(document.createTextNode(message));
+
+    // Get div messages and append
+    const messagesDiv = document.querySelector('.messages');
+    messagesDiv.appendChild(div);
+
+    // Timout afeter 3 seconds
+    setTimeout(function () {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
+  deleteBook(target) {
+    if (target.classList.contains('deleteBtn')) {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  clearFields() {
+    document.getElementById('bookTitle').value = '';
+    document.getElementById('bookAuthor').value = '';
+    document.getElementById('bookIsbn').value = '';
+  }
+}
 
 // Event listener to Add Book
 document.getElementById('book-form').addEventListener('submit', function (e) {
